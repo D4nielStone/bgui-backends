583d9767005dd546dacb356ab78d0fb99eb6625e
D4nielStone <do1167475@gmail.com>
2026-05-17 03:48:09 -0300

    Fix typo in Vulkan debug messenger severity flag

--- vulkan/bgui_backend_vulkan.cpp
+++ vulkan/bgui_backend_vulkan.cpp
@@ -55,7 +55,7 @@ void bgui::create_vk_instance() {
     VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
     debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
     debugCreateInfo.messageSeverity =
-        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERbguiE_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
         VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
         VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
     debugCreateInfo.messageType =
