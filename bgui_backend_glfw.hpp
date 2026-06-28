1fff41a57f792d26ff1fb24dde752253cfbc757e
D4nielStone <do1167475@gmail.com>
2026-06-27 21:07:53 -0300

    same

--- bgui_backend_glfw.hpp
+++ bgui_backend_glfw.hpp
@@ -4,6 +4,7 @@
 
 namespace bgui {
     GLFWwindow* set_up_glfw(int width, int height, const char* title, int flags = 0, GLFWmonitor* monitor = nullptr, GLFWwindow* share = nullptr);
+    void add_glfw_window_position(int x, int y);
     void glfw_main_loop();
     void glfw_update(bgui::context &window_io);
     void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
@@ -11,6 +12,8 @@ namespace bgui {
     void glfw_char_callback(GLFWwindow* window, unsigned int codepoint);
     void glfw_framebuffer_size_callback(GLFWwindow* window, int width, int height);
     void glfw_window_refresh_callback(GLFWwindow* window);
+    void set_glfw_window_decoration(bool b);
+    void set_glfw_window_position(int x, int y);
     void shutdown_glfw();
     bool should_close_glfw();
     void swap_glfw();
